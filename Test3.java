f8c051086a7b1a9cfd89e37b6b3aacc504717b3e
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-06-04 18:00:08 -0500

    Se crea una clase de excepción y una de pruebas.

---
+++ Test3.java
@@ -0,0 +1,25 @@
+public class Test3 {
+  public static void main(String[] args) {
+ 
+    System.out.println("Números:");
+    Queue<Integer> Q = new QueueLink<Integer>();
+    try {
+      Q.add(10);
+      System.out.println(Q);
+      Q.add(14);
+      System.out.println(Q);
+      Q.add(7);
+      System.out.println(Q);
+      Q.add(19);
+      System.out.println(Q);											
+      Q.add(25);
+      System.out.println(Q);
+      System.out.println("Eliminado: " + Q.remove());
+      System.out.println("Se elimina el elemento: " + Q.remove());
+    } catch(ExceptionIsEmpty x	){
+      System.out.println(x.getMessage());
+
+    }
+  }
+}
+
