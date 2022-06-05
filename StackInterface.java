459847c41e4bfd83f1330277e729d95ecf4719e7
AnalyQuispe <gquispeb@unsa.edu.pe>
2022-06-04 19:10:06 -0500

    Interface agregada para la clase Stack.

---
+++ StackInterface.java
@@ -0,0 +1,10 @@
+package Lab_03;
+
+public interface StackInterface<T> {
+	boolean isEmpty();
+	T peek() throws ExceptionIsEmpty;
+	T pop() throws ExceptionIsEmpty;
+	T push(T item);
+	int search(T element);
+
+}
