fd8e7eaf64c6a1e61c543ee96b4cb779ac9bc8f8
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-06-05 03:28:08 -0500

    preparando prueba

---
+++ PilasIguales.java
@@ -0,0 +1,10 @@
+public class PilasIguales{
+
+    public static void main(String[] args) throws ExceptionIsEmpty{
+        int[] arr1 = {1, 1, 1, 2, 3};
+        int[] arr2 = {2, 3, 4};
+        int[] arr3 = {1, 4, 1, 1};
+
+        System.out.println(equalStacks(arr1, arr2, arr3));
+    }
+}
