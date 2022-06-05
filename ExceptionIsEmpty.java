84fe16a7fed4131cfd271df3cfc44839575c9237
AnalyQuispe <gquispeb@unsa.edu.pe>
2022-06-04 19:09:37 -0500

    Clase ExceptionIsEmpty agregada para el manejo de excepciones.

---
+++ ExceptionIsEmpty.java
@@ -0,0 +1,13 @@
+package Lab_03;
+
+public class ExceptionIsEmpty extends Exception {
+
+	public ExceptionIsEmpty() {
+		super();
+	}
+	
+	public ExceptionIsEmpty(String msg) {
+		super(msg);
+	}
+
+}
