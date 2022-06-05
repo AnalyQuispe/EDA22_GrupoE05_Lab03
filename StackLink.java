14b883f3dc63f1a0c0f8ea42d36997875f0eb8e8
ervin <91166138+ifs570@users.noreply.github.com>
2022-06-05 15:02:16 -0500

    Update StackLink.java

--- StackLink.java
+++ StackLink.java
@@ -9,7 +9,7 @@ public class StackLink <E> implements stack <E>{
 		this.tope=new Node<E>(x,this.tope);
 	}
 	
-	public E top() throws ExceptionIsEmpty{
+	public E peek() throws ExceptionIsEmpty{
 	if(isEmpty())
 			throw new ExceptionIsEmpty("Stack esta vacio...");
 		E item=this.tope.getData();
