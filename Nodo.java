fecf34a8fe39fec2fae475e4ff99d9cf47a0f387
amoroccoire <93268964+amoroccoire@users.noreply.github.com>
2022-06-05 22:36:59 -0500

    Update and rename Node.java to Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,12 +1,12 @@
-public class Node<E> {
+public class Nodo<E> {
 	private E data;
-	private Node<E> next;
+	private Nodo<E> next;
 	
-	public Node(E data, Node<E> next) {
+	public Nodo(E data, Nodo<E> next) {
 		this.data=data;
 		this.next=next;
 	}
-	public Node(E data) {
+	public Nodo(E data) {
 		this(data, null);
 	}
 	public E getData() {
@@ -15,7 +15,7 @@ public class Node<E> {
 	public void setData(E data) {
 		this.data=data;
 	}
-	public Node<E> getNext(){
+	public Nodo<E> getNext(){
 		return this.next;
 	}
 }
